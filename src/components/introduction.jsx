b7c02d027ac9b33ce4d79e31a06e86dc3192adba
Miles Kramer <mileskramer@Miless-MBP.localdomain>
2023-03-19 21:04:12 -0500

    look at repo

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -14,7 +14,7 @@ export default class Introduction extends Component {
 			                          <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
 			                            <div className="slider-text-inner js-fullheight">
 			                              <div className="desc">
-			                                <h1>ay ay ay <br />I'm a dill pickle</h1>
+			                                <h1><br />Ideas to make money for the entire year!</h1>
 			                                <p><a className="btn btn-primary btn-learn" href="https://news3lv.com/resources/media2/16x9/full/1015/center/80/ca5c3f64-ad3f-4418-8d00-123eeddc3914-large16x9_AP23059081884291.jpg" target="_blank" rel="noopener noreferrer">View CV<i className="icon-download4" /></a></p>
 			                              </div>
 			                            </div>
@@ -29,7 +29,7 @@ export default class Introduction extends Component {
 			                          <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
 			                            <div className="slider-text-inner">
 			                              <div className="desc">
-			                                <h1>I love building<br /> walls!</h1>
+			                                <h1>Teaches how to make quick money in seconds!<br /> walls!</h1>
 			                                <p><a className="btn btn-primary btn-learn" href="https://github.com/dbarochiya" target="_blank" rel="noopener noreferrer">View Projects <i className="icon-briefcase3" /></a></p>
 			                              </div>
 			                            </div>
@@ -44,8 +44,8 @@ export default class Introduction extends Component {
 			                          <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
 			                            <div className="slider-text-inner">
 			                              <div className="desc">
-			                                <h1>I often <br/>poop </h1>
-			                                <p><a className="btn btn-primary btn-learn" href="https://dhruvbarochiya.me/" target="_blank" rel="noopener noreferrer">View Blog <i className="icon-book" /></a></p>
+								<h1>MMM<br/>Money Making Machine </h1>
+								<p><a className="btn btn-primary btn-learn" href="https://dhruvbarochiya.me/" target="_blank" rel="noopener noreferrer">View Blog <i className="icon-book" /></a></p>
 			                              </div>
 			                            </div>
 			                          </div>

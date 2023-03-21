7e5a9771197f1ad1f39e66c857dbed296daf9179
Miles Kramer <mileskramer@Miless-MBP.localdomain>
2023-03-21 16:53:23 -0500

    look at repo

--- src/components/timeline.jsx
+++ src/components/timeline.jsx
@@ -62,7 +62,7 @@ export default class Info extends Component {
 			            </div>
 			          )
 		   <h1>Summer Jobs</h1>
-<p>there are tons of jobs to do over the summer but here are the main obes</p>
+<p>there are tons of job's to do over the summer but here are the main job's</p>
 <h2>Dog Walking</h2>
 <p>dog walking is a very common way for people to make money because there are tons of people that have dogs in many places</p<
 <h2>Lawn Mowing</h2>

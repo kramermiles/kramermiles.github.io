41bdf981c5c9c272447fe8d4b9248a788044322d
Miles Kramer <mileskramer@Miless-MBP.localdomain>
2023-03-21 20:46:25 -0500

    look at repo

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -26,8 +26,8 @@ export default class About extends Component {
 			              <div className="colorlib-narrow-content">
 			                  <div className="row">
 			                  <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
-			                      <span className="heading-meta">What I do?</span>
-			                      <h2 className="colorlib-heading">Here are some of my expertise</h2>
+			                      <span className="heading-meta"></span>
+			                      <h2 className="colorlib-heading">Common Questions</h2>
 			                  </div>
 			                  </div>
 			                  <div className="row row-pt-md">
--- src/components/timeline.jsx
+++ src/components/timeline.jsx
@@ -21,8 +21,16 @@ export default class Info extends Component {
 			                              <i className="icon-pen2" />
 			                            </div>
 			                            <div className="timeline-label">
-			                              <h2>About This Website<span></span></h2>
-			                              <p>This website is for my P3 Project and I think its pretty cool :D</p>
+			                              <h2>Spring Jobs<span></span></h2>
+			                              <p>jobs to do in spring</p>
+						<h3>Gardening</h3>
+						<p>this is something many people want to do but dont want to do it so you can go over and  maybe pull weeds and water the plants</p>
+						<h3>Car Washing</h3>
+						<p>you can go out and wash someones car and that will make there car shiny and clean</p>
+						<h3>Spring Clean Out</h3>
+						<p>during spring tons of people want to clean out there house and will hire someone to do it</p>
+						<h3>Dog Walking</h3>
+						<p>dog walking can be done in summer aswell but you can do it in spring if you like</p>
 			                            </div>
 			                          </div>
 			                        </article>
@@ -32,10 +40,14 @@ export default class Info extends Component {
                                                       <i className="icon-pen2" />
                                                     </div>
                                                     <div className="timeline-label">
-                                                      <h2>Summer Jobs <span>2023</span></h2>
+                                                      <h2>Summer Jobs <span></span></h2>
                                                       <p>there are a lot of summer jobs that you can do but here are the main ones</p>
                                                       <h3>Dog walking</h3>
                                                       <p>dog walking is an amazing way to make money because so many people have dogs</p>
+							<h3>Stands</h3>
+							<p>over the summer people are bound to be thirsty so you can sell things at a stand the most popular ones are lemonade, water, and iced tea. you can also add some sweets for people to buy</p>
+							<h3>Lawn Mowing</h3>
+					<p>you can mow someones lawn for them and a lot of people don't want to waste there time doing that so they usually just hire someone so thats where you would do it, its okay if you dont have a lawn mower because there are way more jobs on this list</p>
                                                     </div>
                                                   </div>
                                                 </article>
@@ -45,8 +57,12 @@ export default class Info extends Component {
 			                              <i className="icon-pen2" />
 			                            </div>
 			                            <div className="timeline-label">
-			                              <h2>I am an epic man<span>1896-Present</span></h2>
-			                              <p>me so epic so big L</p>
+			                              <h2>Fall Jobs<span></span></h2>
+			                              <p>here are some jobs to do in the fall</p>
+						<h3>Leaf Raking</h3>
+						<p>this is a VERY common thing that people will pay you to do and its not too hard</p>
+						<h3>Apple Cider</h3>
+						<p>in fall tons of people want to drink some nice hot apple cider so you can make a table or stand and sell it</p>
 			                            </div>
 			                          </div>
 			                        </article>
@@ -56,8 +72,16 @@ export default class Info extends Component {
 			                              <i className="icon-pen2" />
 			                            </div>
 			                            <div className="timeline-label">
-			                              <h2>I went to Jane Stenson school for a pretty long time<span>2016-2022</span></h2>
-			                              <p>I have completed my higher secondary education with major subjects as Physics,Chemistry & Maths with 91 merit. During my time at school, I have developed interest in solving complex problems of the fundamental physics which helped me to improve my understanding of any problem and also my mathematical skills to actually solve the problems.</p> 
+			                              <h2>Winter Jobs<span></span></h2>
+			                             <p>here are some jobs you can do through out Winter</p>
+							 <h3>Snow Shoveling</h3> 
+							<p> there are tons of people who just do not want to do there snow shoveling so have just a cheap snow shovel can get you a pretty good profit (its even better if you have a machine that you can use)</p>
+							<h3>Bake Sale>
+ 							<p>this one is very simular to the stand but with cookie and treats which are loved around the Winter holidays and if you want you can also sell hot cocoa</h3>
+						<h3>Sidewalk Salting</h3>
+						<p>side walk salting is something people who hire you for and you can make a sort of deal and do salting and snow shoveling</p>
+						<h3>Pet Sitting</h3> 
+						<p>over the holidays tons of people go on vacation to place and they can't take care of there dog so they have to hire someone that is where you come in</p>
 			                            </div>
 			                          </div>
 			                        </article>

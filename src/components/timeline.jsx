3076d428f602e013859be904ce148845f750c8b6
Miles Kramer <mileskramer@Miless-MBP.localdomain>
2023-03-21 20:47:51 -0500

    look at repo

--- src/components/timeline.jsx
+++ src/components/timeline.jsx
@@ -77,7 +77,7 @@ export default class Info extends Component {
 							 <h3>Snow Shoveling</h3> 
 							<p> there are tons of people who just do not want to do there snow shoveling so have just a cheap snow shovel can get you a pretty good profit (its even better if you have a machine that you can use)</p>
 							<h3>Bake Sale>
- 							<p>this one is very simular to the stand but with cookie and treats which are loved around the Winter holidays and if you want you can also sell hot cocoa</h3>
+ 							<p>this one is very simular to the stand but with cookie and treats which are loved around the Winter holidays and if you want you can also sell hot cocoa</p>
 						<h3>Sidewalk Salting</h3>
 						<p>side walk salting is something people who hire you for and you can make a sort of deal and do salting and snow shoveling</p>
 						<h3>Pet Sitting</h3> 
